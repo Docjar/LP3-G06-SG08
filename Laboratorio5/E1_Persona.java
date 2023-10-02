05efe3dbee6589797aaa028d33f3a2df27ec3d1f
Docjar <128495327+Docjar@users.noreply.github.com>
2023-10-01 23:35:36 -0500

    E1_Persona.java

---
+++ Laboratorio5/E1_Persona.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testcomposicion;
+
+/**
+ *
+ * @author Nadier
+ */
+public class Persona {
+	private int id;
+	private String nombre;
+	private String apellido;
+	private Cuenta cuenta;
+	private char tipoCliente;
+	private String noCu;
+	
+	public static  final int cuentaC= 999;
+	public static final int cuentaB= 4999;
+	public static final int cuentaE= 7999;
+        
+        public static int C = cuentaC;
+        public static int B = cuentaB;
+        public static int E = cuentaE;
+	public String cuen;
+
+	public Persona(int id, String nombre, String apellido, char tipoCliente,Cuenta cuenta) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.tipoCliente = tipoCliente;
+                this.cuenta = cuenta;
+                switch(tipoCliente){
+                    case 'C':
+                        C++;
+                       cuen = tipoCliente+String.valueOf(C);
+                       break;
+                    case 'B':
+                        B++;
+                        cuen = tipoCliente+String.valueOf(B);
+                        break;
+                    case 'E':
+                        E++;
+                        cuen = tipoCliente+String.valueOf(E);
+                        break;
+                    default:
+                        C++;
+                       cuen = 'C'+String.valueOf(C);
+                       break;
+                }
+		
+	}
+	public char getTipoCliente() {
+		return tipoCliente;
+	}
+	public void setTipoCliente(char tc) {
+		tipoCliente = tc;
+	}
+        public Cuenta getCuenta(){
+            return cuenta;
+        }
+	
+	public static char tipoCliente(Persona p1,char tc) {
+		switch(tc) {
+		case 'C':
+			p1.setTipoCliente(tc);
+			return tc;
+		case 'B':
+			p1.setTipoCliente(tc);
+			return tc;
+		case 'E':
+			p1.setTipoCliente(tc);
+			return tc;
+		default:
+			p1.setTipoCliente('C');
+			return tc;
+		}
+		
+	}
+        public void retirar(double cantidad){
+            cuenta.retirar(cantidad);
+        }
+        public void depositar(double cantidad){
+            cuenta.depositar(cantidad);
+        }
+	public String toString() {
+		return "Cliente: " + id + "\n"+"Tipo: "+tipoCliente+"\n"+"Nombres: "+nombre+" "+apellido+"\n"+"No.Cuenta: "+cuen+"\t\t"+"Saldo: "+cuenta.getSaldo()+"\n";
+		
+	}
+	
+}
+
+
