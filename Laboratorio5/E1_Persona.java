70f921feae38d6d9150d599ac27bfb25213b7a70
Docjar <128495327+Docjar@users.noreply.github.com>
2023-10-01 23:36:59 -0500

    E1_Persona.java

--- Laboratorio5/E1_Persona.java
+++ Laboratorio5/E1_Persona.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.testcomposicion;
 
-/**
- *
- * @author Nadier
- */
 public class Persona {
 	private int id;
 	private String nombre;
