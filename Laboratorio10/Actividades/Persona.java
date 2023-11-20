2afddd547d4c372feedb79eb6b70fd04f11e95d8
FaabrizioLS <142517138+FaabrizioLS@users.noreply.github.com>
2023-11-19 22:18:46 -0500

    Add files via upload

---
+++ Laboratorio10/Actividades/Persona.java
@@ -0,0 +1,13 @@
+class Persona {
+    private String dni;
+    private String nombres;
+    private String direccion;
+
+    public Persona(String dni, String nombres, String direccion) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.direccion = direccion;
+    }
+}
+
+
